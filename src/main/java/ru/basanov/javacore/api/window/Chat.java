36965d77f13e73151a749087cb6f19799eb4a5a6
Mingiyan1987 <basanov.mt87@gmail.com>
2018-10-02 21:02:32 +0300

    Подключил jcr

--- src/main/java/ru/basanov/javacore/api/window/Chat.java
+++ src/main/java/ru/basanov/javacore/api/window/Chat.java
@@ -1,6 +1,25 @@
 package ru.basanov.javacore.api.window;
 
 
+import org.jetbrains.annotations.Nullable;
+
+import javax.jcr.Session;
+
 public interface Chat {
+
+    void init();
+
+    void shutdown();
+
+    boolean login();
+
+    boolean logout();
+
+    boolean status();
+
+    boolean save();
+
+    @Nullable
+    Session session = null;
 
 }
